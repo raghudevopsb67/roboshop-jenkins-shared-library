0837f09317fffbf5ae024f2a60fcefa4ec7b668b
Raghu K <raghuk.vit@gmail.com>
2022-10-25 07:27:30 +0530

    Update roboshop-jenkins-shared-library/

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -5,49 +5,9 @@ def call() {
       env.TAG_NAME = ""
     }
 
-      sh 'env'
-      common.codeQuality()
-
-      if ( BRANCH_NAME == "main" || TAG_NAME ==~ ".*" ) {
-        stage('Style Checks') {
-          echo 'Style Checks'
-        }
-      }
-
-//
-//      stage('Unit Tests') {
-//        when {
-//          anyOf {
-//            branch 'main'
-//            tag "*"
-//          }
-//        }
-//        steps {
-//          echo 'Unit tests'
-//        }
-//      }
-//
-//      stage('Download Dependencies') {
-//        when { tag "*" }
-//        steps {
-//          echo 'Download Dependencies'
-//        }
-//      }
-//
-//      stage('Prepare Artifact') {
-//        when { tag "*" }
-//        steps {
-//          echo 'Prepare Artifact'
-//        }
-//      }
-//
-//      stage('Publish Artifact') {
-//        when { tag "*" }
-//        steps {
-//          echo 'Publish Artifact'
-//        }
-//      }
-
+    common.codeQuality()
+    common.styleChecks()
+    common.artifacts()
 
   }
 
