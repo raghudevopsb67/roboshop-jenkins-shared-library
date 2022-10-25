85bf32d748c00424c431c46f5687b648e104dc74
Raghu K <raghuk.vit@gmail.com>
2022-10-25 07:07:07 +0530

    Update roboshop-jenkins-shared-library/

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,59 +1,54 @@
 def call() {
-  pipeline {
-
-    agent any
-
-    stages {
-
-      common.codeQuality
-
-      stage('Style Checks') {
-        when {
-          anyOf {
-            branch 'main'
-            tag "*"
-          }
-        }
-        steps {
-          echo 'Code Quality'
-        }
-      }
-
-      stage('Unit Tests') {
-        when {
-          anyOf {
-            branch 'main'
-            tag "*"
-          }
-        }
-        steps {
-          echo 'Unit tests'
-        }
-      }
-
-      stage('Download Dependencies') {
-        when { tag "*" }
-        steps {
-          echo 'Download Dependencies'
-        }
-      }
-
-      stage('Prepare Artifact') {
-        when { tag "*" }
-        steps {
-          echo 'Prepare Artifact'
-        }
-      }
-
-      stage('Publish Artifact') {
-        when { tag "*" }
-        steps {
-          echo 'Publish Artifact'
-        }
-      }
+  node {
 
 
-    }
+      common.codeQuality()
+//
+//      stage('Style Checks') {
+//        when {
+//          anyOf {
+//            branch 'main'
+//            tag "*"
+//          }
+//        }
+//        steps {
+//          echo 'Code Quality'
+//        }
+//      }
+//
+//      stage('Unit Tests') {
+//        when {
+//          anyOf {
+//            branch 'main'
+//            tag "*"
+//          }
+//        }
+//        steps {
+//          echo 'Unit tests'
+//        }
+//      }
+//
+//      stage('Download Dependencies') {
+//        when { tag "*" }
+//        steps {
+//          echo 'Download Dependencies'
+//        }
+//      }
+//
+//      stage('Prepare Artifact') {
+//        when { tag "*" }
+//        steps {
+//          echo 'Prepare Artifact'
+//        }
+//      }
+//
+//      stage('Publish Artifact') {
+//        when { tag "*" }
+//        steps {
+//          echo 'Publish Artifact'
+//        }
+//      }
+
 
   }
 
