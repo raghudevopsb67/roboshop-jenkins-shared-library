d3d4204c7fc946c641b2ec0dd6a80f48cfe91b81
Raghu K <raghuk.vit@gmail.com>
2022-10-25 07:15:05 +0530

    Update roboshop-jenkins-shared-library/

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -6,7 +6,7 @@ def call() {
 
       if ( BRANCH_NAME == "main" || tag ==~ "*" ) {
         stage('Style Checks') {
-          echo 'Code Quality'
+          echo 'Style Checks'
         }
       }
 
