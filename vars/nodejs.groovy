d57f52b61ff550f9359147b139b8499ae0df2c4b
Raghu K <raghuk.vit@gmail.com>
2022-10-26 07:21:28 +0530

    Update roboshop-jenkins-shared-library/

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,10 +1,13 @@
 def call() {
   node {
-
-    common.codeQuality()
-    common.codeChecks()
-    common.artifacts()
-
+    try {
+      common.codeQuality()
+      common.codeChecks()
+      common.artifacts()
+    } catch (Exception e) {
+      mail bcc: '', body: "Build Failed ${RUN_DISPLAY_URL}", cc: '', from: 'raghuk.vit@gmail.com', replyTo: '', subject: 'BUILD FAILURE', to: 'raghuk.vit@gmail.com'
+    }
+    
   }
 
 }
