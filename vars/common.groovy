c4838953b8bd954813a859e1548863cab41ea3f3
Raghu K <raghuk.vit@gmail.com>
2022-12-02 06:28:16 +0530

    Update roboshop-jenkins-shared-library/

--- vars/common.groovy
+++ vars/common.groovy
@@ -31,7 +31,7 @@ def codeChecks() {
 }
 
 def artifacts() {
-  if ( env.TAG_NAME ==~ ".*" ) {
+  //if ( env.TAG_NAME ==~ ".*" ) {
 
     stage('Prepare Artifacts') {
       if (env.APPTYPE == "nodejs") {
@@ -60,33 +60,19 @@ def artifacts() {
       }
     }
 
-    stage('Publish Artifacts') {
-      withCredentials([usernamePassword(credentialsId: 'NEXUS', passwordVariable: 'nexusPass', usernameVariable: 'nexusUser')]) {
-        sh '''
-          curl -v -u ${nexusUser}:${nexusPass} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://nexus.roboshop.internal:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
-        '''
-      }
-    }
-
-  }
-}
-
-
-def docker() {
-  //if ( env.TAG_NAME ==~ ".*" ) {
-
     stage('Build Docker Image') {
       sh '''
         docker build .
       '''
     }
 
-    stage('Publish Artifacts') {
-      withCredentials([usernamePassword(credentialsId: 'NEXUS', passwordVariable: 'nexusPass', usernameVariable: 'nexusUser')]) {
-        sh '''
-ls        '''
-      }
-    }
+//    stage('Publish Artifacts') {
+//      withCredentials([usernamePassword(credentialsId: 'NEXUS', passwordVariable: 'nexusPass', usernameVariable: 'nexusUser')]) {
+//        sh '''
+//          curl -v -u ${nexusUser}:${nexusPass} --upload-file ${COMPONENT}-${TAG_NAME}.zip http://nexus.roboshop.internal:8081/repository/${COMPONENT}/${COMPONENT}-${TAG_NAME}.zip
+//        '''
+//      }
+//    }
 
   //}
 }
